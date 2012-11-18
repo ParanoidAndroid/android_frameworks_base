1abf800eaf629600efdb1f1a3eddd94033834d47
JBirdVegas <jbirdvegas@gmail.com>
2012-11-18 07:23:30 -0600

    BootMessage: show more info in boot dialog
    Tell the users what package we are running dexOpt on currently
    
    Change-Id: I665ab3847ec1846e40915a1fae21c43e2e7ccf6d

--- core/java/android/view/WindowManagerPolicy.java
+++ core/java/android/view/WindowManagerPolicy.java
@@ -1036,6 +1036,11 @@ public interface WindowManagerPolicy {
     public void systemBooted();
 
     /**
+     * name of package being worked on during boot time message
+     */
+    public void setPackageName(String pkgName);
+
+    /**
      * Show boot time message to the user.
      */
     public void showBootMessage(final CharSequence msg, final boolean always);
