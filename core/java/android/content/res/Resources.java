957ab342da0fcc03e7161a836842c733f00629f1
Paul Henschel <drcmda@gmail.com>
2012-11-18 16:27:59 +0100

    Bugfixes

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -1476,8 +1476,9 @@ public class Resources extends ExtendedPropertiesUtils {
             if (mConfiguration.densityDpi != Configuration.DENSITY_DPI_UNDEFINED) {
                 mMetrics.densityDpi = mConfiguration.densityDpi;
                 mMetrics.density = mConfiguration.densityDpi * DisplayMetrics.DENSITY_DEFAULT_SCALE;
+                mMetrics.paranoidHook();
             }
-            mMetrics.scaledDensity = mMetrics.density * mConfiguration.fontScale;
+            mMetrics.scaledDensity = mMetrics.density * mConfiguration.fontScale;            
 
             String locale = null;
             if (mConfiguration.locale != null) {
@@ -1872,7 +1873,7 @@ public class Resources extends ExtendedPropertiesUtils {
             }
             sPreloaded = true;
             mPreloading = true;
-            sPreloadedDensity = DisplayMetrics.DENSITY_DEVICE;
+            sPreloadedDensity = DisplayMetrics.getDeviceDensity();
             mConfiguration.densityDpi = sPreloadedDensity;
             updateConfiguration(null, null);
         }
