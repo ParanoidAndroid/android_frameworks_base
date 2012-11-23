1e1cecb4d5cf1b5699b98198b52c5d24cc6d46a2
aaronpoweruser <atlrudeboi1@gmail.com>
2012-11-22 23:36:15 -0600

    Remove lag when launching an app and improve scrolling
    Change-Id: I3d8df90b4f9a6d03c6a4158dcf6bf87737dcaef6

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -3185,7 +3185,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         }
     }
 
-    final void logAppTooSlow(ProcessRecord app, long startTime, String msg) {
+    final void logAppTooSlow(int pid, long startTime, String msg) {
         if (true || IS_USER_BUILD) {
             return;
         }
@@ -3223,7 +3223,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                 sb.append(msg);
                 FileOutputStream fos = new FileOutputStream(tracesFile);
                 fos.write(sb.toString().getBytes());
-                if (app == null) {
+                if (pid <= 0) {
                     fos.write("\n*** No application process!".getBytes());
                 }
                 fos.close();
@@ -3233,9 +3233,9 @@ public final class ActivityManagerService extends ActivityManagerNative
                 return;
             }
 
-            if (app != null) {
+            if (pid > 0) {
                 ArrayList<Integer> firstPids = new ArrayList<Integer>();
-                firstPids.add(app.pid);
+                firstPids.add(pid);
                 dumpStackTraces(tracesPath, firstPids, null, null, null);
             }
 
