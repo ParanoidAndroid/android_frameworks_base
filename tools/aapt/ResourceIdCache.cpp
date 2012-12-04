23508591d1767843f737a4f8d6bfb1dc7600cd55
aaronpoweruser <atlrudeboi1@gmail.com>
2012-12-03 23:54:38 -0500

    Revert "Add simple caching to XML compilation"
    This reverts commit 29ef5b0460af17ed981c1ca0249d7a4e14a19cd6.

--- tools/aapt/ResourceIdCache.cpp
+++ tools/aapt/ResourceIdCache.cpp
@@ -14,29 +14,22 @@ struct lutEntry {
     uint32_t resId;
 };
 
-// more than enough - wastes about 100kb of memory (ex strings)
+// more than enough... uses about 100kb of memory
 static const int lutCapacity = 4096;
-static struct lutEntry* lut = NULL;
+static struct lutEntry lut[lutCapacity];
 static int lutUsed = 0;
-
-static bool lutAlloc() {
-    if (lut == NULL) lut = new struct lutEntry[lutCapacity];
-    return (lut != NULL);
-}
 
 uint32_t ResourceIdCache::lookup(const String16& package,
                                  const String16& type,
                                  const String16& name,
                                  bool onlyPublic)
 {
-    if (!lutAlloc()) return 0;
-
     for (int i = 0; i < lutUsed; i++) {
         if (
             // name is most likely to be different
             (name == lut[i].name) &&
-            (type == lut[i].type) &&
             (package == lut[i].package) &&
+            (type == lut[i].type) &&
             (onlyPublic == lut[i].onlyPublic)
         ) {
             return lut[i].resId;
@@ -51,8 +44,6 @@ bool ResourceIdCache::store(const String16& package,
                             bool onlyPublic,
                             uint32_t resId)
 {
-    if (!lutAlloc()) return false;
-
     if (lutUsed < lutCapacity) {
         lut[lutUsed].package = String16(package);
         lut[lutUsed].type = String16(type);
