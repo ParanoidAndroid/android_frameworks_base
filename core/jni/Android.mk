852b82f1a5f73eb641c69eebae6d997ff91232b6
Lucas Crowthers <lucasc@codeaurora.org>
2013-01-08 15:54:49 -0500

    frameworks/base: vectorize Bitmap operations
    Enable gcc vectorizing optimizations for Bitmap operations within
    libandroid_runtime.so.  Adds -ftree-vectorize -mvectorize-with-neon-quad
    -fprefetch-loop-arrays to functions and compiles Bitmap.cpp in ARM mode
    (necessary to enable vectorizing optimizations) for appropriate targets.
    
    Change-Id: I12defef6f47adf41af849df6e750097f5906d92d
    
    Conflicts:
    	core/jni/Android.mk

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -86,7 +86,6 @@ LOCAL_SRC_FILES:= \
 	android_util_XmlBlock.cpp \
 	android_util_PackageRedirectionMap.cpp \
 	android/graphics/AutoDecodeCancel.cpp \
-	android/graphics/Bitmap.cpp \
 	android/graphics/BitmapFactory.cpp \
 	android/graphics/Camera.cpp \
 	android/graphics/Canvas.cpp \
@@ -219,6 +218,27 @@ LOCAL_SHARED_LIBRARIES := \
 	libusbhost \
 	libharfbuzz_ng \
 	libz
+
+ifeq ($(HAVE_SELINUX),true)
+LOCAL_C_INCLUDES += external/libselinux/include
+LOCAL_SHARED_LIBRARIES += libselinux
+LOCAL_CFLAGS += -DHAVE_SELINUX
+endif # HAVE_SELINUX
+
+ifeq ($(TARGET_ARCH), arm)
+  ifeq ($(ARCH_ARM_HAVE_NEON),true)
+    TARGET_arm_CFLAGS += -DUSE_NEON_BITMAP_OPTS -mvectorize-with-neon-quad
+    LOCAL_SRC_FILES+= \
+		android/graphics/Bitmap.cpp.arm
+  else
+    LOCAL_SRC_FILES+= \
+		android/graphics/Bitmap.cpp
+  endif
+else
+    LOCAL_SRC_FILES+= \
+		android/graphics/Bitmap.cpp
+endif
+
 
 ifeq ($(USE_OPENGL_RENDERER),true)
 	LOCAL_SHARED_LIBRARIES += libhwui
