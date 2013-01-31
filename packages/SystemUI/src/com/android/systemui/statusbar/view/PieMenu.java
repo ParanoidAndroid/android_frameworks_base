12526b749faec93d0784ab3fbde8cf7ff7f565d0
drcmda <drcmda@gmail.com>
2013-01-31 14:14:30 +0000

    PIE: preparations for gap
    Change-Id: I1059186b93e31c622f5c3fb4ebdae1b9b27b4060

--- packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/view/PieMenu.java
@@ -138,6 +138,7 @@ public class PieMenu extends FrameLayout {
     private int mPanelOrientation;
     private int mInnerPieRadius;
     private int mOuterPieRadius;
+    private int mPieGap;
     private int mInnerChevronRadius;
     private int mOuterChevronRadius;
     private int mInnerBatteryRadius;
@@ -234,7 +235,17 @@ public class PieMenu extends FrameLayout {
         }
 
         public void start() {
-            if (!manual) animator.start();
+            if (!manual) {
+                animator.setDuration(duration);
+                animator.start();
+            }
+        }
+
+        public void reverse(int milliSeconds) {
+            if (!manual) {
+                animator.setDuration(milliSeconds);
+                animator.reverse();
+            }
         }
 
         public void cancel() {
@@ -243,6 +254,7 @@ public class PieMenu extends FrameLayout {
         }
 
         public int index;
+        public int duration;
         public boolean manual;
         public boolean animateIn;
         public float fraction;
@@ -264,6 +276,7 @@ public class PieMenu extends FrameLayout {
                 Settings.System.PIE_MODE, 2);
         mPieSize = Settings.System.getFloat(mContext.getContentResolver(),
                 Settings.System.PIE_SIZE, 1f);
+        mPieGap = 1; //Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_GAP, 3);
 
         // Snap
         mSnapRadius = (int)(mResources.getDimensionPixelSize(R.dimen.pie_snap_radius) * mPieSize);
@@ -315,8 +328,8 @@ public class PieMenu extends FrameLayout {
             mBatteryJuice.setColor(COLOR_BATTERY_JUICE);
         }
 
-        mStartBattery = mPanel.getDegree() + mEmptyAngle + 1;
-        mEndBattery = mPanel.getDegree() + 88;
+        mStartBattery = mPanel.getDegree() + mEmptyAngle + mPieGap;
+        mEndBattery = mPanel.getDegree() + (mPieGap <= 2 ? 88 : 90 - mPieGap);
         mBatteryPathBackground = makeSlice(mStartBattery, mEndBattery, mInnerBatteryRadius, mOuterBatteryRadius, mCenter);
         mBatteryPathJuice = makeSlice(mStartBattery, mStartBattery + mBatteryLevel * (mEndBattery-mStartBattery) /
                 100, mInnerBatteryRadius, mOuterBatteryRadius, mCenter);
@@ -385,33 +398,33 @@ public class PieMenu extends FrameLayout {
         }
 
         // Linear animators
-        mAnimators[ANIMATOR_DEC_SPEED10].animator.setDuration((int)(mOverallSpeed * 1));
+        mAnimators[ANIMATOR_DEC_SPEED10].duration = (int)(mOverallSpeed * 1);
         mAnimators[ANIMATOR_DEC_SPEED10].animator.setInterpolator(new DecelerateInterpolator());
 
-        mAnimators[ANIMATOR_DEC_SPEED15].animator.setDuration((int)(mOverallSpeed * 1.5));
+        mAnimators[ANIMATOR_DEC_SPEED15].duration = (int)(mOverallSpeed * 1.5);
         mAnimators[ANIMATOR_DEC_SPEED15].animator.setInterpolator(new DecelerateInterpolator());
 
-        mAnimators[ANIMATOR_DEC_SPEED30].animator.setDuration((int)(mOverallSpeed * 3));
+        mAnimators[ANIMATOR_DEC_SPEED30].duration = (int)(mOverallSpeed * 3);
         mAnimators[ANIMATOR_DEC_SPEED30].animator.setInterpolator(new DecelerateInterpolator());
 
-        mAnimators[ANIMATOR_ACC_SPEED10].animator.setDuration((int)(mOverallSpeed * 1));
+        mAnimators[ANIMATOR_ACC_SPEED10].duration = (int)(mOverallSpeed * 1);
         mAnimators[ANIMATOR_ACC_SPEED10].animator.setInterpolator(new AccelerateInterpolator());
 
-        mAnimators[ANIMATOR_ACC_SPEED15].animator.setDuration((int)(mOverallSpeed * 1.5));
+        mAnimators[ANIMATOR_ACC_SPEED15].duration = (int)(mOverallSpeed * 1.5);
         mAnimators[ANIMATOR_ACC_SPEED15].animator.setInterpolator(new AccelerateInterpolator());
 
-        mAnimators[ANIMATOR_ACC_SPEED30].animator.setDuration((int)(mOverallSpeed * 3));
+        mAnimators[ANIMATOR_ACC_SPEED30].duration = (int)(mOverallSpeed * 3);
         mAnimators[ANIMATOR_ACC_SPEED30].animator.setInterpolator(new AccelerateInterpolator());
 
         // Cascade accelerators
         for(int i = ANIMATOR_ACC_INC_1; i < ANIMATOR_ACC_INC_15 + 1; i++) {
-            mAnimators[i].animator.setDuration((int)(mOverallSpeed - (mOverallSpeed * 0.8) / (i + 2)));
+            mAnimators[i].duration = (int)(mOverallSpeed - (mOverallSpeed * 0.8) / (i + 2));
             mAnimators[i].animator.setInterpolator(new AccelerateInterpolator());
             mAnimators[i].animator.setStartDelay(i * mOverallSpeed / 10);
         }
 
         // Special purpose
-        mAnimators[ANIMATOR_BATTERY_METER].animator.setDuration((int)(mOverallSpeed * 1.5));
+        mAnimators[ANIMATOR_BATTERY_METER].duration = (int)(mOverallSpeed * 1.5);
         mAnimators[ANIMATOR_BATTERY_METER].animator.setInterpolator(new DecelerateInterpolator());
 
         mAnimators[ANIMATOR_SNAP_WOBBLE].manual = true;
@@ -573,7 +586,6 @@ public class PieMenu extends FrameLayout {
         float emptyangle = mEmptyAngle * (float)Math.PI / 180;
         int inner = mInnerPieRadius;
         int outer = mOuterPieRadius;
-        int gap = 1;
 
         int lesserSweepCount = 0;
         for (PieItem item : mItems) {
@@ -590,7 +602,7 @@ public class PieMenu extends FrameLayout {
         for (PieItem item : mItems) {
             sweep = ((float) (Math.PI - 2 * emptyangle) / mItems.size()) * (item.isLesser() ? 0.65f : 1 + adjustedSweep);
             angle = (emptyangle + sweep / 2 - (float)Math.PI/2);
-            item.setPath(makeSlice(getDegrees(0) - gap, getDegrees(sweep) + gap, outer, inner, mCenter));
+            item.setPath(makeSlice(getDegrees(0) - mPieGap, getDegrees(sweep) + mPieGap, outer, inner, mCenter));
             View view = item.getView();
 
             if (view != null) {
@@ -640,7 +652,7 @@ public class PieMenu extends FrameLayout {
         }
 
         @Override
-        public void onAnimationUpdate(ValueAnimator animation) {
+        public void onAnimationUpdate(ValueAnimator animation) {            
             mAnimators[mIndex].fraction = animation.getAnimatedFraction();
 
             // Special purpose animators go here
@@ -663,6 +675,7 @@ public class PieMenu extends FrameLayout {
         cancelAnimation();
         invalidate();
         for (int i = 0; i < mAnimators.length; i++) {
+            mAnimators[i].animateIn = true;
             mAnimators[i].start();
         }
     }
