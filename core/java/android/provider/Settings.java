87e57d07df1179b64a34f9022c6f555843f0870d
Eugene San (eugenesan) <eugenesan@gmail.com>
2013-02-19 23:53:41 +0200

    Forward port Quiet Hours settings
    Change-Id: I4a7624a58421d8934eea89a9b7bde4539d3384db

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2426,7 +2426,7 @@ public final class Settings {
          */
         public static final String NAV_BAR_TABUI_MENU = "nav_bar_tabui_menu";
 
-         /**
+        /**
          * Ability to change navigation bar color
          * 0 = P.A.C. is off, it will switch everything color related back to stock and ignore calls
          * 1 = P.A.C. is on, it will listen to calls again
@@ -2434,7 +2434,7 @@ public final class Settings {
          */
         public static final String PER_APP_COLOR = "per_app_color";
 
-         /**
+        /**
          * Ability to change navigation bar color
          * Format: AARRGGBB|AARRGGBB|index[|speed]
          * First color is the primary, systemwide color
@@ -2445,7 +2445,7 @@ public final class Settings {
          */
         public static final String NAV_BAR_COLOR = "nav_bar_color";
 
-         /**
+        /**
          * Ability to change navigation button color
          * Format: AARRGGBB|AARRGGBB|index[|speed]
          * First color is the primary, systemwide color
@@ -2456,7 +2456,7 @@ public final class Settings {
          */
         public static final String NAV_BUTTON_COLOR = "nav_button_color";
 
-         /**
+        /**
          * Widget Buttons to Use
          * Ability to change navigation glow color
          * Format: AARRGGBB|AARRGGBB|index[|speed]
@@ -2571,19 +2571,19 @@ public final class Settings {
          */
         public static final String USER_INTERFACE_STATE = "user_interface_state";
 
-        /**	
+        /**
          * Allows to show the background activity back the lockscreen
          * @hide
          */
         public static final String LOCKSCREEN_SEE_THROUGH = "lockscreen_see_through";
 
-        /**	
+        /**
          * Allows lockscreen to be rotated
          * @hide
          */
         public static final String LOCKSCREEN_ALLOW_ROTATION = "lockscreen_allow_rotation";
 
-        /**	
+        /**
          * Allows lockscreen to show homescreen widgets
          * The value is boolean (1 or 0).
          * @hide
@@ -2602,7 +2602,49 @@ public final class Settings {
          */
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
-        /**	
+        /**
+         * Whether to enable quiet hours.
+         * @hide
+         */
+        public static final String QUIET_HOURS_ENABLED = "quiet_hours_enabled";
+
+        /**
+         * Sets when quiet hours starts. This is stored in minutes from the start of the day.
+         * @hide
+         */
+        public static final String QUIET_HOURS_START = "quiet_hours_start";
+
+        /**
+         * Sets when quiet hours end. This is stored in minutes from the start of the day.
+         * @hide
+         */
+        public static final String QUIET_HOURS_END = "quiet_hours_end";
+
+        /**
+         * Whether to remove the sound from outgoing notifications during quiet hours.
+         * @hide
+         */
+        public static final String QUIET_HOURS_MUTE = "quiet_hours_mute";
+
+        /**
+         * Whether to disable haptic feedback during quiet hours.
+         * @hide
+         */
+        public static final String QUIET_HOURS_HAPTIC = "quiet_hours_haptic";
+
+        /**
+         * Whether to remove the vibration from outgoing notifications during quiet hours.
+         * @hide
+         */
+        public static final String QUIET_HOURS_STILL = "quiet_hours_still";
+
+        /**
+         * Whether to attempt to dim the LED color during quiet hours.
+         * @hide
+         */
+        public static final String QUIET_HOURS_DIM = "quiet_hours_dim";
+
+        /**
          * Defines the shortcuts to be shown on lockscreen
          * Usage is like this: target:icon|target:icon|target:icon
          * if :icon is not set, default application icon will be used
@@ -2635,13 +2677,13 @@ public final class Settings {
          */
         public static final String MAX_NOTIFICATION_ICONS = "max_notification_icons";
 
-        /**	
+        /**
          * Whether to show or not clock on statusbar
          * @hide
          */
         public static final String STATUS_BAR_SHOW_CLOCK = "status_bar_show_clock";
 
-        /**	
+        /**
          * AM/PM style on statusbar, either normal, small or gone.
          * @hide
          */
@@ -2861,6 +2903,12 @@ public final class Settings {
             SIP_CALL_OPTIONS,
             SIP_RECEIVE_CALLS,
             POINTER_SPEED,
+            QUIET_HOURS_ENABLED,
+            QUIET_HOURS_START,
+            QUIET_HOURS_END,
+            QUIET_HOURS_MUTE,
+            QUIET_HOURS_STILL,
+            QUIET_HOURS_DIM,
             VIBRATE_WHEN_RINGING
         };
 
