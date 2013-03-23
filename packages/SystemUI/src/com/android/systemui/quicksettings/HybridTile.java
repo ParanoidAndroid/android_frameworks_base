b917d3ccd0dfe78dc7e5b4d91ddc1667a9e5f767
drcmda <drcmda@gmail.com>
2013-03-23 18:46:18 +0000

    Custom view for hybrid toggle
    displays DPI, Layout and Colors for the foreground app
    
    Change-Id: I8b989365ec7a23bcd72780abf9eeb4e57eafd556

--- packages/SystemUI/src/com/android/systemui/quicksettings/HybridTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/HybridTile.java
@@ -8,6 +8,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Handler;
 import android.provider.Settings;
@@ -16,6 +17,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
+import android.widget.TextView;
+
+import java.math.BigInteger;
 
 import com.android.systemui.R;
 import com.android.systemui.statusbar.phone.QuickSettingsController;
@@ -25,13 +29,16 @@ public class HybridTile extends QuickSettingsTile {
 
     private String mPackagename;
     private String mSourceDir;
+    private String mStatus;
+    private String mColor = "NULL|NULL|NULL|NULL|NULL";
 
     public HybridTile(Context context, LayoutInflater inflater,
             QuickSettingsContainerView container, QuickSettingsController qsc, Handler handler) {
         super(context, inflater, container, qsc);
 
-        mDrawable = R.drawable.ic_qs_hybrid;
         mLabel = context.getString(R.string.quick_settings_hybrid_label);
+        mTileLayout = R.layout.quick_settings_tile_hybrid;
+
         mOnClick = new OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,24 +73,63 @@ public class HybridTile extends QuickSettingsTile {
         PackageManager pm = mContext.getPackageManager();
 
         try {
-            PackageInfo foregroundAppPackageInfo = pm.getPackageInfo(mPackagename, 0);
-            mLabel = foregroundAppPackageInfo.applicationInfo.loadLabel(pm).toString();
+            PackageInfo foregroundAppPackageInfo = pm.
+                    getPackageInfo(mPackagename, 0);
+            mLabel = foregroundAppPackageInfo.applicationInfo.
+                    loadLabel(pm).toString();
 
-            ApplicationInfo appInfo = ExtendedPropertiesUtils.getAppInfoFromPackageName(mPackagename);
+            ExtendedPropertiesUtils.refreshProperties();
+            ApplicationInfo appInfo = ExtendedPropertiesUtils.
+                    getAppInfoFromPackageName(mPackagename);
             mSourceDir = appInfo.sourceDir;
+
+            mStatus = String.valueOf(ExtendedPropertiesUtils.getActualProperty(mPackagename +
+                    ExtendedPropertiesUtils.PARANOID_DPI_SUFFIX)) + " DPI / " +
+                    String.valueOf(ExtendedPropertiesUtils.getActualProperty(mPackagename +
+                    ExtendedPropertiesUtils.PARANOID_LAYOUT_SUFFIX)) + " P";
+
+            mColor = ExtendedPropertiesUtils.getProperty(mPackagename +
+                    ExtendedPropertiesUtils.PARANOID_COLORS_SUFFIX, "NULL|NULL|NULL|NULL|NULL");
+
         } catch(NameNotFoundException Exception) {}
 
-        updateTile();
         updateQuickSettings();
     }
 
-    private synchronized void updateTile() {
 
+    @Override
+    void updateQuickSettings() {
+
+        TextView status = (TextView) mTile.findViewById(R.id.hybrid_status);
+        status.setText(mStatus);
+
+        TextView app = (TextView) mTile.findViewById(R.id.hybrid_app);
+        app.setText(mLabel);
+
+        // Color changes
+
+        View[] swatches = new View[5];
+        swatches[0] = mTile.findViewById(R.id.hybrid_swatch1);
+        swatches[1] = mTile.findViewById(R.id.hybrid_swatch2);
+        swatches[2] = mTile.findViewById(R.id.hybrid_swatch3);
+        swatches[3] = mTile.findViewById(R.id.hybrid_swatch4);
+        swatches[4] = mTile.findViewById(R.id.hybrid_swatch5);
+
+        String[] colors = mColor.split(ExtendedPropertiesUtils.PARANOID_STRING_DELIMITER);
+        if (colors.length == ExtendedPropertiesUtils.PARANOID_COLORS_COUNT) {
+            for(int colorIndex = 0; colorIndex < ExtendedPropertiesUtils.PARANOID_COLORS_COUNT; colorIndex++) {
+                swatches[colorIndex].setBackgroundDrawable(mContext.getResources().getDrawable(
+                        R.drawable.color_picker).mutate());                    
+                swatches[colorIndex].getBackground().setColorFilter(colors[colorIndex]
+                        .toUpperCase().equals("NULL") ? ExtendedPropertiesUtils.PARANOID_COLORCODES_DEFAULTS[
+                colorIndex] : new BigInteger(colors[colorIndex], 16).intValue(),
+                        PorterDuff.Mode.SRC_ATOP);
+            }
+        }
     }
 
     @Override
     void onPostCreate() {
-        updateTile();
         super.onPostCreate();
     }
 
