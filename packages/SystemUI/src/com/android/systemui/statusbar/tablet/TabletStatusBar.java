dd245c3380a53d1a43e6d5c871ac118df30e8a4c
aaronpoweruser <atlrudeboi1@gmail.com>
2013-03-24 18:37:51 -0700

    TABUI: Apply redraw fix
    Change-Id: I7a40617611d1493cab339a1a7a9ee317d5c6216c

--- packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/tablet/TabletStatusBar.java
@@ -196,6 +196,7 @@ public class TabletStatusBar extends BaseStatusBar implements
     KeyEvent mSpaceBarKeyEvent = null;
 
     View mCompatibilityHelpDialog = null;
+    View mDummyView ;
 
     // for disabling the status bar
     int mDisabled = 0;
@@ -573,6 +574,10 @@ public class TabletStatusBar extends BaseStatusBar implements
             }
         } catch (RemoteException ex) {
         }
+
+        // Create a dummy view to force the screen to redraw
+        mDummyView = new View(mContext);
+        mWindowManager.addView(mDummyView, getDummyTriggerLayoutParams(mContext,Gravity.TOP));
 
         // set recents activity navigation bar view
         RecentsActivity.addNavigationCallback(this);
@@ -1726,7 +1731,8 @@ public class TabletStatusBar extends BaseStatusBar implements
         // If the device hasn't been through Setup, we only show system notifications
         for (int i=0; i<N; i++) {
             Entry ent = mNotificationData.get(N-i-1);
-            if (provisioned || showNotificationEvenIfUnprovisioned(ent.notification)) {
+            if (provisioned || showNotificationEvenIfUnprovisioned(ent.notification)
+                && !notificationIsForCurrentUser(ent.notification)) {
                 toShow.add(ent.row);
             }
         }
