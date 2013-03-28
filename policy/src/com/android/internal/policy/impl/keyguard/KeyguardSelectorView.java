d10d0cf6e19498402b2e2c44622309879b301564
Danesh Mondegarian <daneshm90@gmail.com>
2013-03-27 20:48:13 -0400

    LockscreenTargets : Dismiss on target select
    Only dismiss prior to activity launch for lockscreen shortcuts,
    camera and appwidget picker remain untouched.
    
    Change-Id: I2770c628ce0708570aa6ea0eac9e3cd1d82287fb

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardSelectorView.java
@@ -177,6 +177,11 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
         }
 
         @Override
+        protected void dismissKeyguardOnNextActivity() {
+            getCallback().dismiss(false);
+        }
+
+        @Override
         Context getContext() {
             return mContext;
         }};
