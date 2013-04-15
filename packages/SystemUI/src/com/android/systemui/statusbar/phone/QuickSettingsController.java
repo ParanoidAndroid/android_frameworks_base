cee38c3a07ec57dd9cdc4245074746023d5cbbde
Brandon Bennett <bennetb@gmail.com>
2013-04-15 01:49:04 -0600

    QS/PW: Check for mobile data instead of telephony permissions (1/2)
    For QuickSettings and Power Widget check if there is
    ConnectivityManager.TYPE_MOBILE network avaialbe directly instead of relying
    on a checking for the PackageManager.FEATURE_TELEPHONY permission.  Some
    devices like tilapia (Nexus 7 GSM) has mobile data but does not set the
    telephony permission.
    
    Patchset 2:
       	- Use ConnectivityManager.isNetworkSupported() instead of checking for a null
    	  value returned from ConnectivityManager.getNetworkInfo(). Thanks
    	- Have MobileNetworkTile use the QSUtil deviceSupportsMobileData method
    
    Change-Id: I71bd6217cbe08622f805de4ae30a87df11340f5f
    Fixes: CYAN-803
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -41,7 +41,7 @@ import static com.android.internal.util.cm.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.cm.QSConstants.TILE_DESKTOPMODE;
 import static com.android.internal.util.cm.QSConstants.TILE_HYBRID;
 import static com.android.internal.util.cm.QSUtils.deviceSupportsBluetooth;
-import static com.android.internal.util.cm.QSUtils.deviceSupportsTelephony;
+import static com.android.internal.util.cm.QSUtils.deviceSupportsMobileData;
 import static com.android.internal.util.cm.QSUtils.deviceSupportsUsbTether;
 
 import android.content.BroadcastReceiver;
@@ -125,13 +125,13 @@ public class QuickSettingsController {
 
         // Filter items not compatible with device
         boolean bluetoothSupported = deviceSupportsBluetooth();
-        boolean telephonySupported = deviceSupportsTelephony(mContext);
+        boolean mobileDataSupported = deviceSupportsMobileData(mContext);
 
         if (!bluetoothSupported) {
             TILES_DEFAULT.remove(TILE_BLUETOOTH);
         }
 
-        if (!telephonySupported) {
+        if (!mobileDataSupported) {
             TILES_DEFAULT.remove(TILE_WIFIAP);
             TILES_DEFAULT.remove(TILE_MOBILEDATA);
             TILES_DEFAULT.remove(TILE_NETWORKMODE);
@@ -169,14 +169,16 @@ for (String tile : tiles.split("\\|")) {
                 qs = new RingerModeTile(mContext, this);
             } else if (tile.equals(TILE_SYNC)) {
                 qs = new SyncTile(mContext, this);
-            } else if (tile.equals(TILE_WIFIAP) && telephonySupported) {
+            } else if (tile.equals(TILE_WIFIAP) && mobileDataSupported) {
                 qs = new WifiAPTile(mContext, this);
             } else if (tile.equals(TILE_SCREENTIMEOUT)) {
                 qs = new ScreenTimeoutTile(mContext, this);
-            } else if (tile.equals(TILE_MOBILEDATA) && telephonySupported) {
+            } else if (tile.equals(TILE_MOBILEDATA) && mobileDataSupported) {
                 qs = new MobileNetworkTile(mContext, this);
             } else if (tile.equals(TILE_LOCKSCREEN)) {
                 qs = new ToggleLockscreenTile(mContext, this);
+            } else if (tile.equals(TILE_NETWORKMODE) && mobileDataSupported) {
+                qs = new MobileNetworkTypeTile(mContext, this);
             } else if (tile.equals(TILE_AUTOROTATE)) {
                 qs = new AutoRotateTile(mContext, this, mHandler);
             } else if (tile.equals(TILE_AIRPLANE)) {
