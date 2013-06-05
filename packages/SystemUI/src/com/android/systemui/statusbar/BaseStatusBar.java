c39e4211bb9ee296f8a277cfbc911bf6ca38a322
beerbong <yghazi@gmail.com>
2013-06-05 20:46:53 +0200

    Prevent opening layer when it's system or paranoid The transparent layer seems to cause some system actions close (ie, IME switcher). Also, there's no need to launch the layer when we're launching an activity from halo.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1327,7 +1327,7 @@ public abstract class BaseStatusBar extends SystemUI implements
 
             if (mIntent != null) {
 
-                if (mFloat) {
+                if (mFloat && !"android".equals(mPkg) && !"com.paranoid.halo".equals(mPkg)) {
                     Intent transparent = new Intent(mContext, com.android.systemui.Transparent.class);
                     transparent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                     transparent.addFlags(Intent.FLAG_FLOATING_WINDOW);
