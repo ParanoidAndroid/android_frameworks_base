002c578d54fafa5fd5a2f5d821007f502be2d847
Lars Greiss <kufikugel@googlemail.com>
2013-07-05 01:02:58 +0200

    Frameworks: privacy guard fix UI freeze
    randomly sometimes the complete UI freezes when toggle
    privacy guard. Move out the forcekill from syncronized to
    fix it so that it kills the app when all threads are open
    and they can react to the forcekill which cannot result then in unknown states
    
    cheers
    
    Change-Id: I81514dc5ca9149477ee747a7d90720b83f741baa

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -9108,11 +9108,11 @@ public class PackageManagerService extends IPackageManager.Stub {
             }
             pkgSetting.setPrivacyGuard(enabled, userId);
             mSettings.writePackageRestrictionsLPr(userId);
-            try {
-                ActivityManagerNative.getDefault().forceStopPackage(packageName, userId);
-            } catch (RemoteException e) {
-                //nothing
-            }
+        }
+        try {
+            ActivityManagerNative.getDefault().forceStopPackage(packageName, userId);
+        } catch (RemoteException e) {
+            //nothing
         }
     }
 
