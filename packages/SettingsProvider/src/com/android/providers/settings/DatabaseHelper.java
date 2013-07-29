2b6d4e1f7860a6a2047dae1d5ab090f2b2466e23
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-29 15:28:14 +0100

    SettingsProvider: Add a default for the dialer's autocomplete
    ... and flip it on
    
    Change-Id: I9ff8d3fdb682292a89606856c8851b5eed197f06
    
    Conflicts:
    	packages/SettingsProvider/res/values/defaults.xml
    	packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -1982,6 +1982,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadIntegerSetting(stmt, Settings.System.POINTER_SPEED,
                     R.integer.def_pointer_speed);
+
+            loadIntegerSetting(stmt, Settings.System.UI_FORCE_OVERFLOW_BUTTON,
+                    R.integer.def_force_overflow_button);
         } finally {
             if (stmt != null) stmt.close();
         }
@@ -2095,6 +2098,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
             loadBooleanSetting(stmt, Settings.Secure.USER_SETUP_COMPLETE,
                     R.bool.def_user_setup_complete);
+
+            loadIntegerSetting(stmt, Settings.Secure.DIALPAD_AUTOCOMPLETE,
+                    R.integer.def_dialpad_autocomplete);
         } finally {
             if (stmt != null) stmt.close();
         }
