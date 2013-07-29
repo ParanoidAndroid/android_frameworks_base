8825a0fb16e9c8df703044af2f22357a7c6f08f0
Danny Baumann <dannybaumann@web.de>
2013-07-29 14:33:08 +0200

    Re-add lockscreen music controls toggle.
    Change-Id: I70fb6c0d20caad7af01a45b4a9374eb107a554a3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2422,6 +2422,13 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
+        * Whether music controls should be shown on the lockscreen if a supporting
+        * music player is active.
+        * @hide
+        */
+        public static final String LOCKSCREEN_MUSIC_CONTROLS = "lockscreen_music_controls";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
