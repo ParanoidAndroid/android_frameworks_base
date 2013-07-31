9b0f4a42d517a4d799cb12f17f4044f2f3ab7718
Benjamin Poiesz <bpoiesz@google.com>
2013-07-31 08:58:38 -0300

    Custom Carrier Label (1/2)
    Change-Id: Ib3c804b22b3d718d668305fca5f0062cd0eca919
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3170,6 +3170,16 @@ public final class Settings {
         public static final String ENABLE_FAST_POWERON = "enable_fast_poweron";
         
 	/**
+        public static final String VOLUME_KEYS_CONTROL_RING_STREAM = "volume_keys_control_ring_stream";
+
+	/**
+         * Holds the text for the Carrier label. An empty string
+         * will bring the default text back.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label"; 
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
