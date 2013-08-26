a9bdf7f4d77505f54d64aa8c44b5e98eb8adb530
torrented <wallace.ryan10@gmail.com>
2013-08-26 12:08:47 -0300

    ListView Animations (1/2)
    Change-Id: I5e72934c148daf3d3b36a5812f51d29c7fd98c94
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2793,8 +2793,37 @@ public final class Settings {
          */
         public static final String QUIET_HOURS_MUTE = "quiet_hours_mute";
 
+	/**
+         * ListView Animations
+         * 0 == None
+         * 1 == Wave (Left)
+         * 2 == Wave (Right)
+         * 3 == Scale
+         * 4 == Alpha
+         * 5 == Stack (Top)
+         * 6 == Stack (Bottom)
+         * 7 == Translate (Left)
+         * 8 == Translate (Right)
+         */
+        public static final String LISTVIEW_ANIMATION = "listview_animation";
+
         /**
-         * Whether to disable haptic feedback during quiet hours.
+         * ListView Interpolators
+         * 0 == None
+         * 1 == accelerate_interpolator
+         * 2 == decelerate_interpolator
+         * 3 == accelerate_decelerate_interpolator
+         * 4 == anticipate_interpolator
+         * 5 == overshoot_interpolator
+         * 6 == anticipate_overshoot_interpolator
+         * 7 == bounce_interpolator
+         */
+        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+ 	/**
+         * use Alt Activity Resolver Grid
+         * boolean
+         *
          * @hide
          */
         public static final String QUIET_HOURS_HAPTIC = "quiet_hours_haptic";
