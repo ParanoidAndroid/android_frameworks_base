95ee928bc532d9582b39afa03812c68d869785e0
Schubi <schubi@erlangen.ccc.de>
2013-09-12 03:07:44 +0200

    Fix measurement of QS Tiles and revamp landscape handling
    Thanks to kufikugel / SlimRoms for knowing how to.
    
    [log]
    
    Change-Id: I778e10f2b4a30e281eec4deb96980bad192b2f32

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsContainerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsContainerView.java
@@ -56,10 +56,11 @@ public class QuickSettingsContainerView extends FrameLayout {
     void updateResources() {
         Resources r = getContext().getResources();
         mCellGap = r.getDimension(R.dimen.quick_settings_cell_gap);
-        boolean iSlandscape =
-                getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
-        mNumColumns = iSlandscape ? 6 : Settings.System.getInt(getContext().getContentResolver(),
+        mNumColumns = Settings.System.getInt(getContext().getContentResolver(),
                 Settings.System.QUICK_SETTINGS_COLUMNS, 3);
+        if (isLandscape()) {
+            mNumColumns = mNumColumns * 2;
+        }
         requestLayout();
     }
 
@@ -83,6 +84,10 @@ public class QuickSettingsContainerView extends FrameLayout {
                 ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
                 int colSpan = v.getColumnSpan();
                 lp.width = (int) ((colSpan * cellWidth) + (colSpan - 1) * mCellGap);
+
+                if (((mNumColumns > 3) && !isLandscape()) || ((mNumColumns > 6) && isLandscape())) {
+                    lp.height = (lp.width * mNumColumns - 1) / mNumColumns;
+                }
 
                 // Measure the child
                 int newWidthSpec = MeasureSpec.makeMeasureSpec(lp.width, MeasureSpec.EXACTLY);
@@ -154,5 +159,11 @@ public class QuickSettingsContainerView extends FrameLayout {
                 }
             }
         }
+    }
+
+    private boolean isLandscape() {
+        final boolean isLandscape =
+            getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
+        return isLandscape;
     }
 }
