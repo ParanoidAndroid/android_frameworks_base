bd5b88d37a7cb761715620d817bcc035e2b9d1bc
TheMrcool212 <munirmohamadrani@gmail.com>
2013-09-15 22:31:39 +0800

    Frameworks: customizable slim lockscreen background (1/2)
    - color fill (with transparency slider)
    - custom image (with transparency slider)
    - full transparent
    - default
    
    Change-Id: I5506ebb11644d4c31e06380363f19967f9c958b8
    
    	modified:   core/java/android/provider/Settings.java
    	modified:   policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
    	modified:   services/java/com/android/server/wm/WindowAnimator.java
    
    Cherry-picking will cause all files to conflict
    
    https://github.com/temasek/android_frameworks_base/commit/d1bb90a04e842b5c90dce46743de5fc6b441acf2

--- services/java/com/android/server/wm/WindowAnimator.java
+++ services/java/com/android/server/wm/WindowAnimator.java
@@ -241,6 +241,9 @@ public class WindowAnimator {
                 final boolean wasAnimating = winAnimator.mWasAnimating;
                 final boolean nowAnimating = winAnimator.stepAnimationLocked(mCurrentTime);
 
+    int mTransparent = Settings.System.getInt(mContext.getContentResolver(),
+                        Settings.System.LOCKSCREEN_BACKGROUND_VALUE, 3); 
+
                 if (WindowManagerService.DEBUG_WALLPAPER) {
                     Slog.v(TAG, win + ": wasAnimating=" + wasAnimating +
                             ", nowAnimating=" + nowAnimating);
@@ -270,7 +273,8 @@ public class WindowAnimator {
                         }
                         mService.mFocusMayChange = true;
                     }
-                    if (win.isReadyForDisplay()) {
+                    if (mTransparent == 3) {
+                     if (win.isReadyForDisplay()) { 
                         if(Settings.System.getInt(mContext.getContentResolver(),
                                 Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 0) {
                             if (nowAnimating) {
@@ -285,7 +289,9 @@ public class WindowAnimator {
                         } else {
                             mForceHiding = KEYGUARD_NOT_SHOWN;
                         }
-                    }
+                    } else {
+                         mForceHiding = KEYGUARD_NOT_SHOWN;
+                    } 
                     if (WindowManagerService.DEBUG_VISIBILITY) Slog.v(TAG,
                             "Force hide " + mForceHiding
                             + " hasSurface=" + win.mHasSurface
