1e9fd59d51475172d58ba2df79e209989f29b3a2
TheMrcool212 <munirmohamadrani@gmail.com>
2013-09-17 20:45:44 +0800

    Revert "Use boost hints on dexopt"
    This reverts commit b0d2327a104c2ff7eda10ea7ac58460e680cfcff.

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -106,7 +106,6 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.Parcel;
 import android.os.ParcelFileDescriptor;
-import android.os.PowerManager;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.SELinux;
@@ -212,8 +211,6 @@ public class PackageManagerService extends IPackageManager.Stub {
     private static final String INSTALL_PACKAGE_SUFFIX = "-";
 
     private static final int THEME_MAMANER_GUID = 1300;
-
-    private final PowerManager mPm;
 
     static final int SCAN_MONITOR = 1<<0;
     static final int SCAN_NO_DEX = 1<<1;
@@ -1084,8 +1081,6 @@ public class PackageManagerService extends IPackageManager.Stub {
         mSettings.addSharedUserLPw("android.uid.nfc", NFC_UID, ApplicationInfo.FLAG_SYSTEM);
         mSettings.addSharedUserLPw("android.uid.bluetooth", BLUETOOTH_UID, ApplicationInfo.FLAG_SYSTEM);
         mSettings.addSharedUserLPw("android.uid.shell", SHELL_UID, ApplicationInfo.FLAG_SYSTEM);
-
-        mPm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
 
         String separateProcesses = SystemProperties.get("debug.separate_processes");
         if (separateProcesses != null && separateProcesses.length() > 0) {
@@ -3751,7 +3746,6 @@ public class PackageManagerService extends IPackageManager.Stub {
     private int performDexOptLI(PackageParser.Package pkg, boolean forceDex, boolean defer,
             HashSet<String> done) {
         boolean performed = false;
-        mPm.cpuBoost(1500000);
         if (done != null) {
             done.add(pkg.packageName);
             if (pkg.usesLibraries != null) {
