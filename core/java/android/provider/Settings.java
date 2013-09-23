0fb9bbe10cabba5c7272d8f73faef5e08aedfed6
nims11xda <nirmitkarsanbhaipatel@gmail.com>
2013-09-23 13:35:06 +0200

    derp

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2470,12 +2470,6 @@ public final class Settings {
         public static final String LOCKSCREEN_DISABLED = "lockscreen.disabled";
 
         /**
-         * Stores values for custom lockscreen targets
-         * @hide
-         */
-        public static final String LOCKSCREEN_TARGETS = "lockscreen_targets";
-
-        /**
          * Whether to display notifications on screen when screen is off
          * @hide
          */
