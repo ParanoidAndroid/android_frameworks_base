7466c7fb0196be30bfed1428de598676b24656a0
slickrick <rsalinas1@mail.usf.edu>
2013-10-07 00:30:46 -0700

    Revert these 2 Active Display commits
    Revert "Active-D: Make GlowPadView center the current notification icon for us"
    
    This reverts commit 20bde1981c74f816cfc29a9c3560caca177ee032.
    
    Revert "Active display: Catch Resources.NotFoundException"
    
    This reverts commit f982154c1098cdd245b5231aca955e738b7da1d2.
    
    Change-Id: I8e93ddc98d589e3808924a90bd2a748c5c496275

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/activedisplay/ActiveDisplayView.java
@@ -116,6 +116,7 @@ public class ActiveDisplayView extends FrameLayout {
     private GlowPadView mGlowPadView;
     private View mRemoteView;
     private View mClock;
+    private ImageView mCurrentNotificationIcon;
     private FrameLayout mRemoteViewLayout;
     private FrameLayout mContents;
     private ObjectAnimator mAnim;
@@ -218,6 +219,7 @@ public class ActiveDisplayView extends FrameLayout {
         }
 
         public void onReleased(final View v, final int handle) {
+            ObjectAnimator.ofFloat(mCurrentNotificationIcon, "alpha", 1f).start();
             doTransition(mOverflowNotifications, 1.0f, 0);
             if (mRemoteView != null) {
                 ObjectAnimator.ofFloat(mRemoteView, "alpha", 0f).start();
@@ -231,6 +233,7 @@ public class ActiveDisplayView extends FrameLayout {
             // prevent the ActiveDisplayView from turning off while user is interacting with it
             cancelTimeoutTimer();
             restoreBrightness();
+            ObjectAnimator.ofFloat(mCurrentNotificationIcon, "alpha", 0f).start();
             doTransition(mOverflowNotifications, 0.0f, 0);
             if (mRemoteView != null) {
                 ObjectAnimator.ofFloat(mRemoteView, "alpha", 1f).start();
@@ -450,6 +453,7 @@ public class ActiveDisplayView extends FrameLayout {
 
         mRemoteViewLayout = (FrameLayout) contents.findViewById(R.id.remote_content_parent);
         mClock = contents.findViewById(R.id.clock_view);
+        mCurrentNotificationIcon = (ImageView) contents.findViewById(R.id.current_notification_icon);
 
         mOverflowNotifications = (LinearLayout) contents.findViewById(R.id.keyguard_other_notifications);
         mOverflowNotifications.setOnTouchListener(mOverflowTouchListener);
@@ -851,7 +855,6 @@ public class ActiveDisplayView extends FrameLayout {
                     }
                 } catch (RemoteException re) {
                 } catch (NameNotFoundException nnfe) {
-                } catch (Resources.NotFoundException e) {
                 }
             }
         });
@@ -954,10 +957,7 @@ public class ActiveDisplayView extends FrameLayout {
         try {
             Context pkgContext = mContext.createPackageContext(sbn.getPackageName(), Context.CONTEXT_RESTRICTED);
             mNotificationDrawable = pkgContext.getResources().getDrawable(sbn.getNotification().icon);
-            TargetDrawable centerDrawable = new TargetDrawable(getResources(),createCenterDrawable(mNotificationDrawable));
-            centerDrawable.setScaleX(0.9f);
-            centerDrawable.setScaleY(0.9f);
-            mGlowPadView.setCenterDrawable(centerDrawable);
+            mCurrentNotificationIcon.setImageDrawable(mNotificationDrawable);
             setHandleText(sbn);
             mNotification = sbn;
             mGlowPadView.post(new Runnable() {
@@ -969,7 +969,6 @@ public class ActiveDisplayView extends FrameLayout {
                 }
             });
         } catch (NameNotFoundException e) {
-        } catch (Resources.NotFoundException e) {
         }
     }
 
@@ -1023,13 +1022,6 @@ public class ActiveDisplayView extends FrameLayout {
         stateListDrawable.addState(TargetDrawable.STATE_INACTIVE, handle);
         stateListDrawable.addState(TargetDrawable.STATE_ACTIVE, handle);
         stateListDrawable.addState(TargetDrawable.STATE_FOCUSED, handle);
-        return stateListDrawable;
-    }
-
-    private Drawable createCenterDrawable(Drawable handle) {
-        StateListDrawable stateListDrawable = new StateListDrawable();
-        stateListDrawable.addState(TargetDrawable.STATE_INACTIVE, handle);
-        
         return stateListDrawable;
     }
 
